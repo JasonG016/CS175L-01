dc0af5b19a30fdc7d1938060d3b36e5b077a030c
JasonG016 <114097282+JasonG016@users.noreply.github.com>
2023-02-06 12:39:57 -0500

    Restaurants

---
+++ restaurants.py
@@ -0,0 +1,31 @@
+#Jason Galvao
+#CS 175-01L
+#restaurants
+
+vegetarian=False
+vegan=False
+glutenFree=False
+
+vegetarianInput = input("Is anyone in your party a vegetarian(yes/no)?")
+if vegetarianInput == "yes":
+    vegetarian=True
+    
+veganInput = input("Is anyone in your party a vegan(yes/no)?")
+if veganInput == "yes":
+    vegan=True
+
+glutenFreeInput = input("Is anyone in your party gluten free(yes/no)?")
+if glutenFreeInput == "yes":
+    glutenFree=True
+            
+
+if vegetarian==False and vegan==False and glutenFree==False:
+    print("Joe's Gourmet Burgers")
+elif vegetarian==True and vegan==False and glutenFree==False:
+    print("Mama's Fine Italian")
+elif vegetarian==True and vegan==False and glutenFree==True:
+    print("Main Street Pizza")
+
+print("Corner Cafe")
+print("Chef's Kitchen")
+            
