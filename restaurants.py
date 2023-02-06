77955c4cc9559932069ec7579196f31b024d9d37
JasonG016 <114097282+JasonG016@users.noreply.github.com>
2023-02-06 12:57:45 -0500

    restaurants

--- restaurants.py
+++ restaurants.py
@@ -1,31 +1,33 @@
 #Jason Galvao
 #CS 175-01L
-#restaurants
+#restaurants V2
+keepGoing = "yes"
+while keepGoing == "yes":
+    vegetarian=False
+    vegan=False
+    glutenFree=False
 
-vegetarian=False
-vegan=False
-glutenFree=False
-
-vegetarianInput = input("Is anyone in your party a vegetarian(yes/no)?")
-if vegetarianInput == "yes":
-    vegetarian=True
+    vegetarianInput = input("Is anyone in your party a vegetarian(yes/no)?")
+    if vegetarianInput == "yes":
+        vegetarian=True
     
-veganInput = input("Is anyone in your party a vegan(yes/no)?")
-if veganInput == "yes":
-    vegan=True
+    veganInput = input("Is anyone in your party a vegan(yes/no)?")
+    if veganInput == "yes":
+        vegan=True
 
-glutenFreeInput = input("Is anyone in your party gluten free(yes/no)?")
-if glutenFreeInput == "yes":
-    glutenFree=True
+    glutenFreeInput = input("Is anyone in your party gluten free(yes/no)?")
+    if glutenFreeInput == "yes":
+        glutenFree=True
             
 
-if vegetarian==False and vegan==False and glutenFree==False:
-    print("Joe's Gourmet Burgers")
-elif vegetarian==True and vegan==False and glutenFree==False:
-    print("Mama's Fine Italian")
-elif vegetarian==True and vegan==False and glutenFree==True:
-    print("Main Street Pizza")
+    if vegetarian==False and vegan==False and glutenFree==False:
+        print("Joe's Gourmet Burgers")
+    elif vegetarian==True and vegan==False and glutenFree==False:
+        print("Mama's Fine Italian")
+    elif vegetarian==True and vegan==False and glutenFree==True:
+        print("Main Street Pizza")
 
-print("Corner Cafe")
-print("Chef's Kitchen")
-            
+    print("Corner Cafe")
+    print("Chef's Kitchen")
+
+    keepGoing = input("Do you want to keep going(yes/no)?")
